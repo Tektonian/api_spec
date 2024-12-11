d60f7c80b787abf6ac8468b08b15a328198e5160
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2024-12-11 11:36:04 +0900

    api spec 업데이트

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,5 @@ export type * as RecommendType from "./recommend/recommend"
 export type * as RequestType from "./service/Request"
 export type * as StudentReviewType from "./service/StudentReview"
 export type * as VerificationType from "./service/Verification"
+
+export type * as SearchSchoolType from "./search/School"
