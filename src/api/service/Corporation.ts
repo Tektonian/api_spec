85fb0629fe6926d8f66d144a384c8bd9543b518a
Min Jeong <jungm8453@gmail.com>
2024-12-17 09:31:15 +0900

    Corporation 타입 수정

---
+++ src/api/service/Corporation.ts
@@ -0,0 +1,26 @@
+import { Tspec } from "tspec";
+import { RequestHandler } from "express";
+
+import { ReqCreateCorpProfile, ResCreateCorpProfile } from "../../types/service/Corporation";
+import { ReqGetCorpProfile, ResGetCorpProfile } from "../../types/service/Corporation";
+
+export type CorporationAPISpec = Tspec.DefineApiSpec<{
+    tags: ['Corporation'],
+    basePath: '/api/corporations',
+    paths: {
+        "/": {
+            post: {
+                summary: "Create Corporation",
+                description: "",
+                handler: RequestHandler<undefined, ResCreateCorpProfile, ReqCreateCorpProfile>
+            }
+        },
+        "/:corp_id": {
+            get: {
+                summary: "Get Corporation information",
+                description: "",
+                handler: RequestHandler<ReqGetCorpProfile, ResGetCorpProfile>
+            }
+        },
+    }
+}>
--- src/types/service/Corporation.ts
+++ src/types/service/Corporation.ts
@@ -1,9 +1,58 @@
 import { Tspec } from "tspec"
 
+interface Corporation {
+    corp_id: number;
+    corp_name: string;
+    nationality: string;
+    corp_domain?: string;
+    ceo_name?: string;
+    corp_address?: string;
+    phone_number?: string;
+    corp_num: number;
+    biz_num?: number;
+    biz_started_at?: string;
+    corp_status?: string;
+    biz_type?: string;
+    logo_image?: string;
+    site_url?: string;
+}
+
+interface RequestCard {
+    title: string;
+    reward_price: number;
+    currency: "USD" | "KRW" | "JPY" | "";
+    address: string;
+    start_date: Date;
+    renderLogo: boolean;
+    logo_image?: string;
+}
+
+export interface ReqCreateCorpProfile {
+    corp_id: number;
+    corp_name: string;
+    nationality: string;
+    corp_domain?: string;
+    ceo_name?: string;
+    corp_address?: string;
+    phone_number?: string;
+    corp_num: number;
+    biz_num?: number;
+    biz_started_at?: string;
+    corp_status?: string;
+    biz_type?: string;
+    logo_image?: string;
+    site_url?: string;
+}
+
+export interface ResCreateCorpProfile {
+    corp_id: string;
+}
+
 export interface ReqGetCorpProfile {
     corp_num: number;
 }
 
 export interface ResGetCorpProfile {
-    
+    corp: Corporation;
+    past_requests: RequestCard[];
 }
