67361b960db452954d5428aedaac272e237fa5cc
Min Jeong <jungm8453@gmail.com>
2025-01-03 12:01:23 +0900

    Update User.ts

--- src/types/service/User.ts
+++ src/types/service/User.ts
@@ -3,6 +3,7 @@ import * as Joi from "@hapi/joi";
 import { USER_GENDER_ENUM } from "../../enum/service/User";
 import { COUNTRY_CODE_ENUM } from "../../enum/service/CountryCode";
 import { ReqCreateUserSchema } from "../../joi/service/User";
+import { CONSUMER_ENUM } from "../../enum/service/Consumer";
 
 interface UserData {
     username: string;
@@ -10,7 +11,11 @@ interface UserData {
     // gender?
     nationality: COUNTRY_CODE_ENUM;
     working_country: COUNTRY_CODE_ENUM;
-    roles: string[];
+    roles: CONSUMER_ENUM[];
+    student_id?: number;
+    consumer_id?: number;
+    corp_id?: number;
+    orgn_id?: number;
     // image
 }
 
